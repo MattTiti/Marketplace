7a75ab09a7f3935be060a1b24cb6db8bae3a1b99
Matthew Titi <matthew.titi@icloud.com>
2022-06-15 16:05:31 -0400

    Initial Commit

---
+++ Message/MessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageApp.swift
+//  Message
+//
+//  Created by Matthew Titi on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
